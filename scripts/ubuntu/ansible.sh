e5bae9487aa3fe79fd0d47f980e500c5969c93c4
Jesús García Crespo <jesus@sevein.com>
2016-02-10 11:58:39 -0800

    packer: install ansible with official ppa

--- scripts/ubuntu/ansible.sh
+++ scripts/ubuntu/ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-apt-get install -y python-software-properties
-apt-add-repository ppa:rquillo/ansible
+apt-get install -y software-properties-common python-software-properties
+apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible
